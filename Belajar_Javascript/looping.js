c4c23b012a278ef0c7ace57816a01ce1d1465ec7
Khoiru Riski <khoiruriski2354@gmail.com>
2024-06-23 13:57:43 +0700

    Menambah Perulangan array

--- Belajar_Javascript/looping.js
+++ Belajar_Javascript/looping.js
@@ -1,25 +1,32 @@
 // PERULANGAN
 // TIPE-TIPE PERULANGAN
 
-// FOR
-for(let i = 1; i <=10; i++ ){
-    console.log("Saya Ada Di Dalam Loop")
-    console.log(i);
-}
-for(let i = 0; i <=20; i+=2 ){
-    console.log(i);
-}
-// (-)
-for(let i = 20; i >=0; i-- ){
-    console.log(i);
-}
-// (*)
-for(let i = 10; i <=1000; i*=10 ){
-    console.log(i);
-}
+// // FOR
+// for(let i = 1; i <=10; i++ ){
+//     console.log("Saya Ada Di Dalam Loop")
+//     console.log(i);
+// }
+// for(let i = 0; i <=20; i+=2 ){
+//     console.log(i);
+// }
+// // (-)
+// for(let i = 20; i >=0; i-- ){
+//     console.log(i);
+// }
+// // (*)
+// for(let i = 10; i <=1000; i*=10 ){
+//     console.log(i);
+// }
 
-for(let i = 10; i <=100; i**1 ){
-    console.log(i);
+// for(let i = 10; i <=100; i**1 ){
+//     console.log(i);
+// }
+// PERULANGAN ARRAY
+const animals =['Kucing','Rusa','kambing','Sapi'];
+
+for(let i = 0; i < animals.length; i++){
+    // console.log(i);
+    console.log(i,animals[i]);
 }
 // WHILE
 
